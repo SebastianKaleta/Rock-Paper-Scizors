23a1bd40afc9218eb37589b62b5bfb0b24815222
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-17 21:36:44 +0100

    Add

---
+++ game.js
@@ -0,0 +1,22 @@
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0
+}
+
+const game = {
+    playerHand: "",
+    aiHand: ""
+}
+
+const hands = [...document.querySelectorAll('.select img')];
+
+//Pierwsza funkcja
+function handSelection() {
+    game.playerHand = this.dataset.option;
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 4px red';
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
