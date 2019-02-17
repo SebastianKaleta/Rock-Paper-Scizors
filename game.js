08eab8c2d39f123e2563cf54c49e5180431a34ca
Sebastian <sebastian.m.kaleta@gmail.com>
2019-02-17 21:51:28 +0100

    Add ai selections

--- game.js
+++ game.js
@@ -19,4 +19,21 @@ function handSelection() {
     this.style.boxShadow = '0 0 0 4px red';
 }
 
+//Funkcja wyboru ai
+function aiChoise() {
+    const aiHand = hands[Math.floor(Math.random() * 3)].dataset.option;
+
+    return aiHand;
+}
+
+//funkcja sterująca
+function startGame() {
+    if (!game.playerHand) return alert("Wybierz dłoń!!")
+
+    game.aiHand = aiChoise();
+
+}
+
 hands.forEach(hand => hand.addEventListener('click', handSelection))
+
+document.querySelector('.start').addEventListener('click', startGame)
